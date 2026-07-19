069394373b6449b5e63ff737c05312564f60f588
elitehubseych <elitehubseych@gmail.com>
2026-07-19 14:26:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-USER_TOKEN = os.environ["USER_TOKEN"]
+USER_TOKEN = os.environ.get("USER_TOKEN", "")
 CHAT_READ = int(os.environ.get("CHAT_READ", "2000000020"))
 CHAT_WRITE = int(os.environ.get("CHAT_WRITE", "2000000206"))
 ROLE_CHECKER_ID = int(os.environ.get("ROLE_CHECKER_ID", "-218136766"))
