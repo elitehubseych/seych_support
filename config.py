69f881601b7eafcecf3f4c6ffeaa2ab899b2c6dc
elitehubseych <elitehubseych@gmail.com>
2026-07-21 18:57:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 GROUP_TOKEN = os.environ.get("GROUP_TOKEN", "").strip()
 CHAT_READ = int(os.environ.get("CHAT_READ", "2000000008"))
-CHAT_WRITE = int(os.environ.get("CHAT_WRITE", "2000000007"))
+CHAT_WRITE = int(os.environ.get("CHAT_WRITE", "2000000206"))
 CONFIRM_CODE = os.environ.get("CONFIRM_CODE", "746458b7")
 ROLE_CHECKER_ID = int(os.environ.get("ROLE_CHECKER_ID", "-218136766"))
+DEVELOPER_ID = int(os.environ.get("DEVELOPER_ID", "532796366"))
